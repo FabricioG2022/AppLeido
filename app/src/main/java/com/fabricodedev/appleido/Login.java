0ff682963af01c562460e4a256b330ef0f8666e2
FabricioG2022 <firgodoy@hotmail.com>
2025-09-22 14:32:53 -0300

    feat: inicio de desarrollo del login con el logo y campos de ingreso de datos

--- app/src/main/java/com/fabricodedev/appleido/Login.java
+++ app/src/main/java/com/fabricodedev/appleido/Login.java
@@ -1,8 +1,6 @@
 package com.fabricodedev.appleido;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,20 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ImageButton miBoton;
-        miBoton = findViewById(R.id.imageButton5);
-        miBoton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Login.class);
-            startActivity(intent);
-        });
-
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
